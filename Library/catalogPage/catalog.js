9b0adfb0afdf751ea4367d6efbe2476edbf31834
Salvar Cafarov <salvarcafarov@gmail.com>
2023-03-12 03:34:06 +0400

    lk

--- Library/catalogPage/catalog.js
+++ Library/catalogPage/catalog.js
@@ -22,56 +22,7 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-const allBooksWrap = document.querySelector(".owl-stage");
-console.log(allBooksWrap)
-async function getInfo() {
-  onValue(ref(db, "/Books"), async (sp) => {
-    let z = await sp.val();
-    let arr = Object.entries(z);
-    allBooksWrap.innerHTML =""
-
-    arr.forEach((e) => {
-      allBooksWrap.innerHTML += `<div class="item">
-      <div class="item-inner">
-        <div class="img"><img src="photos/image 6.png" alt=""></div>
-        <div class="name">1111111111111111111111</div>
-        <div class="author">SSSSSSS</div>
-        <div class="readmore"><button>Read More</button></div>
-      </div>
-    </div>`;
-    });
-   await $(".owl1").owlCarousel({
-      margin: 30,
-      animateOut: "fadeOut",
-      animateIn: "fadeIn",
-      loop: true,
-      center: false,
-      mergeFit: true,
-      items: 5,
-      autoplay: true,
-      duration: "3000",
-      dots: false,
-      autoplayHoverPause: true,
-      autoplayTimeout: 3000,
-      dotsEach: true,
-      responsive: {
-        4: { items: 1 },
-        576: { items: 2 },
-        700: { items: 3 },
-        900: { items: 4 },
-        1100: { items: 5 },
-      },
-      nav:true
-    
-    });
-  });
-}
-
-
-
-
-
- $(".owl1").owlCarousel({
+$(".owl1").owlCarousel({
   margin: 30,
   animateOut: "fadeOut",
   animateIn: "fadeIn",
@@ -92,8 +43,7 @@ async function getInfo() {
     900: { items: 4 },
     1100: { items: 5 },
   },
-  nav:true
-
+  nav: true,
 });
 $(".owl2").owlCarousel({
   margin: 30,
@@ -116,7 +66,69 @@ $(".owl2").owlCarousel({
     900: { items: 4 },
     1100: { items: 5 },
   },
-  nav:true
-
+  nav: true,
 });
-getInfo();
+const allBooksWrap = document.querySelector(".owl1");
+console.log(allBooksWrap);
+function resetOwl() {
+  let owlLength = $(".owl1 .item").length;
+  for (let i = 0; i < owlLength; i++) {
+    $(".owl1")
+      .trigger("remove.owl.carousel", [i])
+      .trigger("refresh.owl.carousel");
+  }
+}
+  onValue(ref(db, "/Books"), async (sp) => {
+    let z = await sp.val();
+    let arr = Object.entries(z);
+    resetOwl();
+    arr.forEach((e) => {
+      console.log(e);
+      let all = `<div class="item">
+      <div class="item-inner">
+        <div class="img"><img src="${e[1].bookimgUrl}" alt=""></div>
+        <div class="name">${e[1].bookName}</div>
+        <div class="author">${e[1].authorname}</div>
+        <div class="readmore"><button>Read More</button></div>
+      </div>
+    </div>`;
+     if(e[1].sell){
+      let bestseller =`<div class="item">
+      <div class="item-inner">
+        <div class="img"><img src="${e[1].bookimgUrl}" alt=""></div>
+        <div class="name">${e[1].bookName}</div>
+        <div class="author">${e[1].authorname}</div>
+        <div class="readmore"><button>Read More</button></div>
+      </div>
+    </div>`
+    $(".owl2").trigger("add.owl.carousel", [all]);
+    $(".owl2").trigger("refresh.owl.carousel");
+
+     }
+     e[1].publish
+     if(e[1].publish){
+      let bestseller =`<div class="item">
+      <div class="item-inner">
+        <div class="img"><img src="${e[1].bookimgUrl}" alt=""></div>
+        <div class="name">${e[1].bookName}</div>
+        <div class="author">${e[1].authorname}</div>
+        <div class="readmore"><button>Read More</button></div>
+      </div>
+    </div>`
+    $(".owl2").trigger("add.owl.carousel", [all]);
+    $(".owl2").trigger("refresh.owl.carousel");
+
+     }
+
+
+
+
+
+
+      $(".owl1").trigger("add.owl.carousel", [all]);
+      $(".owl1").trigger("refresh.owl.carousel");
+     
+    });
+  });
+
+
